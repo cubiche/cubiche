4b8f762090f4c2e6faf121e7d7f2242e8e3154a3
Ivannis <ivannis.suarez@gmail.com>
2016-01-25 17:44:58 +0000

    update licence text

--- src/Cubiche/Domain/Core/DomainObjectInterface.php
+++ src/Cubiche/Domain/Core/DomainObjectInterface.php
@@ -1,7 +1,12 @@
 <?php
 
-/**
- * This file is part of the cubiche/cubiche project.
+/*
+ * This file is part of the Cubiche package.
+ *
+ * Copyright (c) Cubiche
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 namespace Cubiche\Domain\Core;
 
