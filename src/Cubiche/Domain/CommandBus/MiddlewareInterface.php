8ec0505cd1c858206dc522359777951bfb30da51
ivannis <ivannis.suarez@gmail.com>
2016-04-08 12:53:19 +0000

    renamed CommandBus namespace

---
+++ src/Cubiche/Domain/CommandBus/MiddlewareInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the Cubiche/CommandBus package.
+ *
+ * Copyright (c) Cubiche
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Cubiche\Domain\CommandBus;
+
+/**
+ * Middleware interface.
+ *
+ * @author Ivannis Suárez Jerez <ivannis.suarez@gmail.com>
+ */
+interface MiddlewareInterface
+{
+    /**
+     * @param object   $command
+     * @param callable $next
+     *
+     * @return mixed
+     */
+    public function execute($command, callable $next);
+}
