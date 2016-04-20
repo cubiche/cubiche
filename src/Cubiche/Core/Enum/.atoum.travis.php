d2461caab8a4f05fcf17d77511dc77343b77b1f8
ivannis <ivannis.suarez@gmail.com>
2016-04-20 15:30:38 +0000

    ignore tests in php 7.0

--- src/Cubiche/Core/Enum/.atoum.travis.php
+++ src/Cubiche/Core/Enum/.atoum.travis.php
@@ -12,23 +12,29 @@ use Cubiche\Tests\Report\Coverage\Coveralls;
 /* @var \mageekguy\atoum\configurator $script */
 $script->addDefaultReport();
 
-if ($token = getenv('COVERALLS_REPO_TOKEN')) {
-    $coverallsReport = new Coveralls(__DIR__, $token);
-
-    $defaultFinder = $coverallsReport->getBranchFinder();
-    $coverallsReport
-        ->setBranchFinder(function () use ($defaultFinder) {
-            if (($branch = getenv('TRAVIS_BRANCH')) === false) {
-                $branch = $defaultFinder();
-            }
-
-            return $branch;
-        })
-        ->setServiceName(getenv('TRAVIS') ? 'travis-ci' : null)
-        ->setServiceJobId(getenv('TRAVIS_JOB_ID') ?: null)
-        ->addDefaultWriter()
+if (getenv('TRAVIS_PHP_VERSION') === '7.0') {
+    $script
+        ->php('php -n -ddate.timezone=Europe/Madrid')
+        ->noCodeCoverage()
     ;
+} else {
+    if ($token = getenv('COVERALLS_REPO_TOKEN')) {
+        $coverallsReport = new Coveralls(__DIR__, $token);
 
-    /* @var \mageekguy\atoum\runner $runner */
-    $runner->addReport($coverallsReport);
+        $defaultFinder = $coverallsReport->getBranchFinder();
+        $coverallsReport
+            ->setBranchFinder(function () use ($defaultFinder) {
+                if (($branch = getenv('TRAVIS_BRANCH')) === false) {
+                    $branch = $defaultFinder();
+                }
+
+                return $branch;
+            })
+            ->setServiceName(getenv('TRAVIS') ? 'travis-ci' : null)
+            ->setServiceJobId(getenv('TRAVIS_JOB_ID') ?: null)
+            ->addDefaultWriter();
+
+        /* @var \mageekguy\atoum\runner $runner */
+        $runner->addReport($coverallsReport);
+    }
 }
