5a22475431b0b6334cb7dbc282812e3600430853
karel <osorioramirez@gmail.com>
2016-05-22 08:47:19 +0000

    fixed projections

--- src/Cubiche/Core/Projection/Projections.php
+++ src/Cubiche/Core/Projection/Projections.php
@@ -10,10 +10,6 @@
  */
 namespace Cubiche\Core\Projection;
 
-use Cubiche\Core\Selector\Custom;
-use Cubiche\Core\Selector\Key;
-use Cubiche\Core\Selector\Method;
-use Cubiche\Core\Selector\Property as PropertySelector;
 use Cubiche\Core\Selector\SelectorInterface;
 
 /**
@@ -33,50 +29,14 @@ class Projections
         return new PropertyProjectionBuilder($selector);
     }
 
-    public static function forAll($selector, $projection)
-    {
-        $forEach = new ForEachProjection($selector, $projection);
-
-        return $forEach;
-    }
-
     /**
-     * @param string $key
+     * @param SelectorInterface   $selector
+     * @param ProjectionInterface $projection
      *
-     * @return \Cubiche\Core\Projection\Property
+     * @return \Cubiche\Core\Projection\ForEachProjection
      */
-    public static function key($key)
+    public static function forAll(SelectorInterface $selector, ProjectionInterface $projection)
     {
-        return new Key($key);
-    }
-
-    /**
-     * @param string $property
-     *
-     * @return \Cubiche\Core\Projection\Property
-     */
-    public static function property($property)
-    {
-        return new PropertySelector($property);
-    }
-
-    /**
-     * @param string $method
-     *
-     * @return \Cubiche\Core\Projection\Property
-     */
-    public static function method($method)
-    {
-        return new Method($method);
-    }
-
-    /**
-     * @param callable $callable
-     *
-     * @return \Cubiche\Core\Projection\Property
-     */
-    public static function custom(callable $callable)
-    {
-        return new Property(new Custom($callable));
+        return new ForEachProjection($selector, $projection);
     }
 }
